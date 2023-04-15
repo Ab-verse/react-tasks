504e7820025c1173d0d2982aac52b13be227e29b
Ab-verse <abhisheksckm17@gmail.com>
2023-04-15 20:43:10 +0530

    add simple-project-1

---
+++ my-app-1/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import Main from "./components/Main";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
