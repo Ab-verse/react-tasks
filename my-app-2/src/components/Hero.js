8384bef3ab2aba22d068bb938f5abcfb5f83aec9
Ab-verse <abhisheksckm17@gmail.com>
2023-04-17 00:27:15 +0530

    build airbnb simple project

---
+++ my-app-2/src/components/Hero.js
@@ -0,0 +1,17 @@
+import React from "react";
+import gridImg from "../assets/Group 77.png";
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <img src={gridImg} alt="gridImg" className="hero--photo" />
+      <h1 className="hero--header">Online Experiences</h1>
+      <p className="hero--text">
+        Join unique interactive activities led by one-of-a-kind hosts-all
+        without leaving home.
+      </p>
+    </section>
+  );
+};
+
+export default Hero;
